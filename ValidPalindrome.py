f18fea82c389a026babbbfad7f63ef8628965b2e
Quinten Krauth <quintenkrauth19@gmail.com>
2023-12-21 16:46:13 -0500

    leetcode practice py

---
+++ ValidPalindrome.py
@@ -0,0 +1,32 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward.
+# Alphanumeric characters include letters and numbers.
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+# Example 1:
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+
+class Solution:
+    def isPalindrome(self, s):
+        s = s.lower()
+
+        # remove non alphanumeric characters
+        s = "".join(char for char in s if char.isalnum())
+
+        # create pointers and check if string is equal on both sides
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+    
+my_solution = Solution()
+
+result = my_solution.isPalindrome("A man, a plan, a canal: Panama")
+
+print(result)
---
+++ ValidParenthesis.py
@@ -0,0 +1,40 @@
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+# An input string is valid if:
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+
+# Example 1:
+
+# Input: s = "()"
+# Output: true
+# Example 2:
+
+# Input: s = "()[]{}"
+# Output: true
+
+class Solution:
+    def isValid(self, s):
+        # lets use a STACK to keep track of opening brackets. A stack is good because it has LIFO properties
+        stack = []
+
+        # define a mapping of opening to closing brackets
+        bracket_mapping = {")": "(", "}": "{", "]": "["} # dictionary
+
+        for char in s:
+            if char in bracket_mapping.values(): # checks if char is an opening bracket
+                stack.append(char)
+            elif char in bracket_mapping.keys(): # checks if char is a closing bracket
+                if not stack or stack.pop() != bracket_mapping[char]: #.pop removes and returns value at the LAST index (-1)
+                    return False 
+            else:
+                return False
+        
+        return not stack # if stack is empty, all brackets were matched/closed properly
+        # NOT STACK = means empty stack
+
+my_solution = Solution()
+
+result = my_solution.isValid("()[]{}")
+
+print(result)
