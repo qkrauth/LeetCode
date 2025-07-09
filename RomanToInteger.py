3bfe978b8cb33e8bdef65177162544a8745ce790
Quinten Krauth <quintenkrauth19@gmail.com>
2025-07-09 18:57:00 -0400

    work

--- RomanToInteger.py
+++ RomanToInteger.py
@@ -10,7 +10,7 @@
 class Solution():
     def romanToInt(self, s):
 
-        # create dict to store values to roman lettering
+        # create dict to store values to roman lettering.
         roman_num_dict = {
             "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000
         }
