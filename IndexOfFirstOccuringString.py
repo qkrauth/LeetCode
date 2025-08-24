f83fd71f3d482fddec6042ff24eb3f7b447aae89
Quinten Krauth <quintenkrauth19@gmail.com>
2025-08-24 19:19:52 -0400

    problems

--- IndexOfFirstOccuringString.py
+++ IndexOfFirstOccuringString.py
@@ -13,7 +13,7 @@
 
 # Input: haystack = "leetcode", needle = "leeto"
 # Output: -1
-# Explanation: "leeto" did not occur in "leetcode", so we return -1.
+# Explanation: "leeto" did not occur in "leetcode", so we return -1
 
 class Solution:
     def firstStr(self, haystack, needle):
