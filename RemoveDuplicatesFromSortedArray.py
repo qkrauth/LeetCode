f9e157968cc9455391c686da4df1a2c2a0116e24
Quinten Krauth <quintenkrauth19@gmail.com>
2025-09-13 13:20:29 -0400

    weekends

--- RemoveDuplicatesFromSortedArray.py
+++ RemoveDuplicatesFromSortedArray.py
@@ -3,7 +3,7 @@
 # The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 # Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
 # Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially.
-# The remaining elements of nums are not important as well as the size of nums.
+# The remaining elements of nums are not important as well as the size of nums
 # Return k.
 
 # Two pointer technique
