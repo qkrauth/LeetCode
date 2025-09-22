136c4d03659e85f9f1382a97e9c19d80c15b87ba
Quinten Krauth <quintenkrauth19@gmail.com>
2025-09-22 17:34:59 -0400

    interview

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -1,5 +1,5 @@
 # 206
-# Given the head of a singly linked list, reverse the list, and return the reversed list
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
 
 # Example 1:
 # Input: head = [1,2,3,4,5]
